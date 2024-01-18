68b2a3803f003bf912fd5476a5e3a5d59d363196
rajeswari <rajeswari.v@thrymr.net>
2024-01-18 12:11:50 +0530

    removing commented code

--- src/main/java/com/example/Phone/Pay/management/service/GitMergeServiceImpl.java
+++ src/main/java/com/example/Phone/Pay/management/service/GitMergeServiceImpl.java
@@ -65,38 +65,5 @@ public class GitMergeServiceImpl implements GitMergeService{
         return "Sorry Something went wrong";
     }
 
- /*   public  String mergeBranches(GitCredentialsDto gitCredentialsDto) {
-        try {
-            try (Git git = Git.open(new File(gitCredentialsDto.getRepository()))) {
-                Repository repository = git.getRepository();
-                Ref sourceRef = repository.findRef("refs/heads/" + gitCredentialsDto.getSourceBranch());
-                if (sourceRef == null) {
-                    System.out.println("Source branch does not exist: " + gitCredentialsDto.getSourceBranch());
-                    return "Source branch does not exist: " + gitCredentialsDto.getSourceBranch();
-                }
-                Ref targetRef = repository.findRef("refs/heads/" + gitCredentialsDto.getTargetBranch());
-                if (targetRef == null) {
-                    System.out.println("Target branch does not exist: " + gitCredentialsDto.getTargetBranch());
-                    return "Target branch does not exist: " + gitCredentialsDto.getTargetBranch();
-                }
-                git.checkout().setName(gitCredentialsDto.getTargetBranch()).call();
-                MergeResult mergeResult = git.merge()
-                        .include(repository.resolve(gitCredentialsDto.getSourceBranch()))
-                        .setCommit(true)
-                        .setFastForward(MergeCommand.FastForwardMode.NO_FF)
-                        .call();
-                if (mergeResult.getMergeStatus().isSuccessful()) {
-                    System.out.println("Merge successful");
-                    return "Successfully Merged";
-                } else {
-                    System.out.println("Merge failed");
-                    return "Merge Failed";
-                }
-            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Sorry Something went wrong";
-    }*/
 }
